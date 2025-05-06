a385323a3754d0bec824ee0a1a8e9e4a5412b9b3
tarit001 <taritmandal001@gmail.com>
2025-05-06 11:30:21 +0530

    hello man 8

--- server/server.js
+++ server/server.js
@@ -16,17 +16,22 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error('MongoDB connection error:', err));
 
 
+// Health check route for Render or general monitoring
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'API is running.....✅ 🚀' });
+  });
+
+
+// Mount your routes after the health check
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/protected', require('./routes/protectedRoutes'));
+
 
 //error handler
 app.use((err, req, res, next) => {
     console.error('Unhandled error:', err);
     res.status(500).json({ message: 'Internal server error' });
 });
-
-app.use('/api/auth', require('./routes/authRoutes'));
-
-app.use('/api/protected', require('./routes/protectedRoutes'));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
